fe5bd9a8e2728066570a244995efc33bc61bca53
abhi <contact@abhishekchouhan.com>
2024-11-24 00:22:09 +0530

    add tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 function App() {
-    return <div></div>;
+    return (
+        <div className="min-h-screen flex items-center justify-center bg-blue-100">
+            <h1 className="text-4xl font-bold text-blue-600">Welcome to School Admin App</h1>
+        </div>
+    );
 }
 
 export default App;
